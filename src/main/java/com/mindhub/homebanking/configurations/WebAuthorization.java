8c11b156253d5b91d7300f7779f019c6dddbfba0
Sebastian Alfonso <alfonsomario2000@gmail.com>
2023-02-07 22:08:41 -0300

    HomeBanking

--- src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
@@ -25,10 +25,10 @@ public class WebAuthorization extends WebSecurityConfigurerAdapter {
                         "/api/clients/current/cards", "/api/transactions", "/api/pdf/generate").hasAuthority("CLIENT")
                 .antMatchers("/rest/**", "/api/**", "/h2-console/**", "/manager.html", "/manager.js" ).hasAuthority("ADMIN")
                 .antMatchers("/web/accounts.html", "/web/accounts.js","/web/account.html",
-                             "/web/account.js", "/web/cards.html","/web/cards.js", "/web/transfers.html", "/web/transfers.js",
-                             "/web/loan-application.html","/web/loan-application.js" , "/web/products.html", "/web/products.js").hasAuthority("CLIENT")
-                .antMatchers(  "/web/login.html", "/web/login.js", "/web/signin.html", "/web/signin.js","/web/index.html" ,
-                        "/web/index.js" , "/web/css/style.css","/web/css/style.css","/web/img/**","/web/img/**","/api/**","/api/clients" ).permitAll()
+                             "/Web/account.js", "/Web/cards.html","/Web/cards.js", "/Web/transfers.html", "/Web/transfers.js",
+                             "/Web/loan-application.html","/Web/loan-application.js" , "/Web/products.html", "/Web/products.js").hasAuthority("CLIENT")
+                .antMatchers(  "/Web/login.html", "/Web/login.js", "/Web/signin.html", "/Web/signin.js","/Web/index.html" ,
+                        "/Web/index.js" , "/Web/css/style.css","/Web/css/style.css","/Web/img/**","/Web/img/**","/api/**","/api/clients" ).permitAll()
                 .anyRequest().denyAll();
 
         http.formLogin()
